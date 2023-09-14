909f8bbb59d5278452053238486f87677413b458
Jeongseok Kang <jskang@lablup.com>
2023-09-14 14:38:25 +0900

    Revert "fix: Presigned URL's endpoint cannot be changed"
    This reverts commit 5fece61c7a42a34670494de359a48f80b6972391.

--- dist/index.js
+++ dist/index.js
@@ -47904,23 +47904,102 @@ const input = {
 };
 
 const minioClient = new minio.Client({
-  endPoint: "10.82.230.111",
+  endPoint: "127.0.0.1",
   port: 9000,
   useSSL: false,
   accessKey: "minioadmin",
   secretKey: "minioadmin",
 });
 
+/**
+ * 
+ * @param {string} bucketName 
+ */
+const bucketExists = async (bucketName) => {
+  return new Promise((resolve, reject) => {
+    minioClient.bucketExists(bucketName, (err, exists) => {
+      if (err) {
+        console.error(err);
+        return reject(err);
+      }
+      if (exists === false) {
+        console.error(`Bucket(${bucketName}) does not exist.`);
+        return resolve(false);
+      }
+      console.log(`Bucket(${bucketName}) exists.`);
+      return resolve(true);
+    });
+  });
+};
+
+/**
+ * 
+ * @param {string} bucketName
+ * @param {string} region
+ */
+const makeBucket = async (bucketName, region = "us-east-1") => {
+  return new Promise((resolve, reject) => {
+    minioClient.makeBucket(bucketName, region, (err) => {
+      if (err) {
+        console.error(`Error creating bucket(${bucketName}).`, err);
+        return reject(err);
+      }
+      return resolve();
+    });
+  });
+};
+
+/**
+ * 
+ * @param {string} bucketName 
+ * @param {string} objectName 
+ * @param {string} filePath 
+ * @param {object} metaData 
+ */
+const fPutObject = async (bucketName, objectName, filePath, metaData) => {
+  return new Promise((resolve, reject) => {
+    minioClient.fPutObject(bucketName, objectName, filePath, metaData, (err, objInfo) => {
+      if (err) {
+        console.error(err);
+        return reject(err);
+      }
+      console.log("Success", objInfo.etag, objInfo.versionId);
+      return resolve(objInfo);
+    });
+  });
+};
+
+/**
+ * 
+ * @param {string} httpMethod
+ * @param {string} bucketName 
+ * @param {string} objectName 
+ * @param {number} expiry 
+ * @param {object} reqParams 
+ * @param {Date} requestDate 
+ * @returns 
+ */
+const presignedUrl = async (httpMethod, bucketName, objectName, expiry, reqParams, requestDate) => {
+  return new Promise((resolve, reject) => {
+    minioClient.presignedUrl(httpMethod, bucketName, objectName, expiry, reqParams, requestDate, (err, presignedUrl) => {
+      if (err) {
+        console.error(err);
+        return reject(err);
+      }
+      return resolve(presignedUrl);
+    });
+  });
+};
+
 async function main(args) {
   const targetName = args.target || path.basename(args.artifact);
   try {
-    const isBucketExist = await minioClient.bucketExists(BUCKET_NAME);
+    const isBucketExist = await bucketExists(BUCKET_NAME);
     if (isBucketExist === false) {
-      await minioClient.makeBucket(BUCKET_NAME);
+      await makeBucket(BUCKET_NAME);
     }
-    const objInfo = await minioClient.fPutObject(BUCKET_NAME, targetName, args.artifact, {});
-    console.log("Success", objInfo.etag, objInfo.versionId);
-    const url = await minioClient.presignedUrl("GET", BUCKET_NAME, targetName, EXPIRY_IN_SECONDS, objInfo, new Date());
+    const objInfo = await fPutObject(BUCKET_NAME, targetName, args.artifact, {});
+    const url = await presignedUrl("GET", BUCKET_NAME, targetName, EXPIRY_IN_SECONDS, objInfo, new Date());
     console.log(`Presign: {"method": "GET", "bucket": "${BUCKET_NAME}", url: ${url}}`);
     core.setOutput("result", "success");
     core.setOutput("url", url);
