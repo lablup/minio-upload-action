409c336a35d5292d0e3bbeac0cce58cb9d9258e3
Jeongseok Kang <jskang@lablup.com>
2023-09-14 14:39:43 +0900

    fix: `fPutObject`'s callback cannot be null

--- dist/index.js
+++ dist/index.js
@@ -47904,50 +47904,12 @@ const input = {
 };
 
 const minioClient = new minio.Client({
-  endPoint: "127.0.0.1",
+  endPoint: "10.82.23.0.111",
   port: 9000,
   useSSL: false,
   accessKey: "minioadmin",
   secretKey: "minioadmin",
 });
-
-/**
- * 
- * @param {string} bucketName 
- */
-const bucketExists = async (bucketName) => {
-  return new Promise((resolve, reject) => {
-    minioClient.bucketExists(bucketName, (err, exists) => {
-      if (err) {
-        console.error(err);
-        return reject(err);
-      }
-      if (exists === false) {
-        console.error(`Bucket(${bucketName}) does not exist.`);
-        return resolve(false);
-      }
-      console.log(`Bucket(${bucketName}) exists.`);
-      return resolve(true);
-    });
-  });
-};
-
-/**
- * 
- * @param {string} bucketName
- * @param {string} region
- */
-const makeBucket = async (bucketName, region = "us-east-1") => {
-  return new Promise((resolve, reject) => {
-    minioClient.makeBucket(bucketName, region, (err) => {
-      if (err) {
-        console.error(`Error creating bucket(${bucketName}).`, err);
-        return reject(err);
-      }
-      return resolve();
-    });
-  });
-};
 
 /**
  * 
@@ -47969,37 +47931,15 @@ const fPutObject = async (bucketName, objectName, filePath, metaData) => {
   });
 };
 
-/**
- * 
- * @param {string} httpMethod
- * @param {string} bucketName 
- * @param {string} objectName 
- * @param {number} expiry 
- * @param {object} reqParams 
- * @param {Date} requestDate 
- * @returns 
- */
-const presignedUrl = async (httpMethod, bucketName, objectName, expiry, reqParams, requestDate) => {
-  return new Promise((resolve, reject) => {
-    minioClient.presignedUrl(httpMethod, bucketName, objectName, expiry, reqParams, requestDate, (err, presignedUrl) => {
-      if (err) {
-        console.error(err);
-        return reject(err);
-      }
-      return resolve(presignedUrl);
-    });
-  });
-};
-
 async function main(args) {
   const targetName = args.target || path.basename(args.artifact);
   try {
-    const isBucketExist = await bucketExists(BUCKET_NAME);
+    const isBucketExist = await minioClient.bucketExists(BUCKET_NAME);
     if (isBucketExist === false) {
-      await makeBucket(BUCKET_NAME);
+      await minioClient.makeBucket(BUCKET_NAME);
     }
     const objInfo = await fPutObject(BUCKET_NAME, targetName, args.artifact, {});
-    const url = await presignedUrl("GET", BUCKET_NAME, targetName, EXPIRY_IN_SECONDS, objInfo, new Date());
+    const url = await minioClient.presignedUrl("GET", BUCKET_NAME, targetName, EXPIRY_IN_SECONDS, objInfo, new Date());
     console.log(`Presign: {"method": "GET", "bucket": "${BUCKET_NAME}", url: ${url}}`);
     core.setOutput("result", "success");
     core.setOutput("url", url);
